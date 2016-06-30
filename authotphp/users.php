4bab352bb0729a6db968dbf402ccc94d86c6cb73
Messager Morgan <messager.morgan.83@gmail.com>
2016-06-30 15:18:11 +0200

    Update users.php

--- authotphp/users.php
+++ authotphp/users.php
@@ -6,6 +6,5 @@
 #
 # Format:
 #
-# login : keypin:Real Name:email:groups,comma,seperated
-user1:AERUKZ4JAERUKZ4J1234:root:morgan.driver@hotmail.fr:admin,user:
-root:AERUKZ4JAERUKZ4J1234:root:morgan.driver@hotmail.fr:admin,user:
+# login:keypin:Real Name:email:groups,comma,seperated
+deleteme:AERUKZ4JAERUKZ4J1234:root:yourmail@exemple.com:admin,user:
