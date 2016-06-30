80f32227cf61c915e1453d5e5af6c3d5cedfb839
Messager Morgan <messager.morgan.83@gmail.com>
2016-06-30 15:18:54 +0200

    Update users.php

--- authotphp/users.php
+++ authotphp/users.php
@@ -6,5 +6,5 @@
 #
 # Format:
 #
-# login:keypin:Real Name:email:groups,comma,seperated
+# login:keypin:Real Name:email:groups,comma,seperated:
 deleteme:AERUKZ4JAERUKZ4J1234:root:yourmail@exemple.com:admin,user:
